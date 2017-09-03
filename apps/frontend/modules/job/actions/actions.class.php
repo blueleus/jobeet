7ec463e87955be0c9cab690ec8facbd69b6b8c84
Andres Orejuela - Blueleus <andresforejuelab@hotmail.com>
2017-09-03 03:05:04 -0500

    feat: Symfony Documentation Jobeet: Day 5 y 6
    - Set up custom routing for module job.
    - I refactored the code in order to separate the controller
    and model layers well.
    - Configuration variables was created at the application level.
    - Jobeet.class.php was created like container class of static methods.
    - PHP code is implemented in the jobs.yml file to generate test
    data more quickly and without repetitions of code.

--- apps/frontend/modules/job/actions/actions.class.php
+++ apps/frontend/modules/job/actions/actions.class.php
@@ -12,9 +12,20 @@ class jobActions extends sfActions
 {
   public function executeIndex(sfWebRequest $request)
   {
-    $this->jobeet_jobs = Doctrine_Core::getTable('JobeetJob')
-      ->createQuery('a')
-      ->execute();
+    //La lógica para obtener los puestos de trabajo activos está ahora 
+    //en el modelo, donde pertenerce
+    // $q = Doctrine_Query::create()
+    //   ->from('JobeetJob j')
+    //   ->where('j.expires_at > ?', date('Y-m-d h:i:s', time()));
+    // $this->jobeet_jobs = $q->execute();
+    
+    // $this->jobeet_jobs = Doctrine_Core::getTable('JobeetJob')->getActiveJobs();
+    
+    //Hasta ahora, no teníamos la categoría en cuenta. De los requisitos, 
+    //la página de inicio debe mostrar los puestos de trabajo por categoría. 
+    //Primero, necesitamos obtener todas las categorías con al menos un puesto 
+    //de trabajo activo.
+    $this->categories = Doctrine_Core::getTable('JobeetCategory')->getWithJobs();
   }
 
   public function executeShow(sfWebRequest $request)
--- lib/model/doctrine/JobeetJobTable.class.php
+++ lib/model/doctrine/JobeetJobTable.class.php
@@ -7,13 +7,38 @@
  */
 class JobeetJobTable extends Doctrine_Table
 {
-    /**
-     * Returns an instance of this class.
-     *
-     * @return object JobeetJobTable
-     */
-    public static function getInstance()
+  /**
+   * Returns an instance of this class.
+   *
+   * @return object JobeetJobTable
+   */
+  public static function getInstance()
+  {
+      return Doctrine_Core::getTable('JobeetJob');
+  }
+
+  public function retrieveActiveJob(Doctrine_Query $q)
+  {
+    $q->andWhere('a.expires_at > ?', date('Y-m-d h:i:s', time()));
+ 
+    return $q->fetchOne();
+  }
+
+  public function getActiveJobs(Doctrine_Query $q = null)
+  {
+    // $q = $this->createQuery('j')
+    //   ->where('j.expires_at > ?', date('Y-m-d h:i:s', time()))
+    //   ->orderBy('j.expires_at DESC');
+ 
+    if (is_null($q))
     {
-        return Doctrine_Core::getTable('JobeetJob');
+      $q = Doctrine_Query::create()
+        ->from('JobeetJob j');
     }
+   
+    $q->andWhere('j.expires_at > ?', date('Y-m-d h:i:s', time()))
+      ->addOrderBy('j.expires_at DESC');
+
+    return $q->execute();
+  }
 }
